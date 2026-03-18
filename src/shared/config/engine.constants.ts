d9ba34ae53afa07c5f70491fc80b5c09a66a8be1
Sergej Siemens <ccmoiseev@gmail.com>
2026-03-18 18:16:41 +0100

    nn-37f18f62d772.nnue 3,5 MB

--- src/shared/config/engine.constants.ts
+++ src/shared/config/engine.constants.ts
@@ -1,1 +1,2 @@
-export const DEFAULT_NNUE_FILE = 'nn-4ca89e4b3abf.nnue'
+// export const DEFAULT_NNUE_FILE = 'nn-4ca89e4b3abf.nnue' // 15 MB
+export const DEFAULT_NNUE_FILE = 'nn-37f18f62d772.nnue' // 4 MB
